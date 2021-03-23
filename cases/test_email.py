342f60ed06d20e36d91338f428d86b4b4c6c3c86
liuyue <>
2021-03-23 09:54:27 +0800

    修改邮件文本

--- cases/test_email.py
+++ cases/test_email.py
@@ -32,7 +32,7 @@ class Test_Email():
         subject = '测试完成'
 
         # 编写HTML类型的邮件正文（把HTML代码写进入）
-        msg = MIMEText('<html><body><a href="">测试已经完成，请到report文件夹查看</a></p></body></html>', 'html', 'utf-8')
+        msg = MIMEText('<html><body><a href="">测试已经完成，请到Jenkins服务器查看allure报告</a></p></body></html>', 'html', 'utf-8')
         msg['Subject'] = Header(subject, 'utf-8')
 
         # 连接发送邮件（smtplib模块基本使用格式）
